5a56133be64049097511f044cc3408cf32be253c
Richard Shelton <richard.shelton567@gmail.com>
2019-12-12 11:30:03 -0500

    Docs Controller/Route Creation Pt 1

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,30 @@
+class DocsController < ApplicationController
+    def index
+    end
+
+    def show 
+    end 
+
+    def new 
+    end 
+    
+    # Update's DB with changes
+    def update 
+    end 
+
+    def edit 
+    end
+
+    def create
+    end
+    
+    def destroy 
+    end 
+
+    private
+        def find_doc
+        end
+
+        def doc_params
+        end 
+end
