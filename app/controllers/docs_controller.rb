e005964f3e898c678a34d5a6a0addf2f8d73a82a
Richard Shelton <richard.shelton567@gmail.com>
2019-12-16 16:47:14 -0500

    Additional Views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,11 +1,17 @@
 class DocsController < ApplicationController
+    # before_action runs the method find_doc only on the show, edit, update, and destroy routes. find_doc is defined on line 36
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
     def index
+        # New -> Old documents display order
+        @docs = Doc.all.order('create_at DESC')
     end
 
     def show 
     end 
 
     def new 
+        # In new.html.haml > _form.html.haml, this is how those templates know what @doc is
         @doc = Doc.new
     end 
     
@@ -31,6 +37,7 @@ class DocsController < ApplicationController
 
     private
         def find_doc
+            @doc = Doc.find(params[:id])
         end
 
         def doc_params
